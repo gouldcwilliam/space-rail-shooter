5b045d25c7c16b7bc02fd8d1cd75ff70c765b47c
chad gould <gouldcwilliam@gmail.com>
2018-06-01 23:24:49 -0400

    power bar updates powerbar indicator updates based on POWERBARLEVEL variable

--- HUD.gd
+++ HUD.gd
@@ -18,29 +18,34 @@ func _ready():
 func _process(delta):
 	if Input.is_key_pressed(KEY_T):
 		POWERUPBARLEVEL+=1
+	if Input.is_key_pressed(KEY_Y):
+		POWERUPBARLEVEL-=1
 	updatePowerBarLevel()
 	pass
-
 
 func updatePowerBarLevel():
 	if POWERUPBARLEVEL > 0 :
 		$Sprite0.set_texture(powerUpBar_FullTexture)
-	elif POWERUPBARLEVEL == 0:
-		$Sprite1.set_texture(powerUpBar_EmptyTexture)
+#	elif POWERUPBARLEVEL == 0:
+#		$Sprite0.set_texture(powerUpBar_EmptyTexture)
 	else:
-		$Sprite1.set_texture(powerUpBar_EmptyTexture)
+		$Sprite0.set_texture(powerUpBar_EmptyTexture)
+		
 	if POWERUPBARLEVEL > 1:
 		$Sprite1.set_texture(powerUpBar_FullTexture)
 	else:
 		$Sprite1.set_texture(powerUpBar_EmptyTexture)
+		
 	if POWERUPBARLEVEL > 2:
 		$Sprite2.set_texture(powerUpBar_FullTexture)
 	else:
 		$Sprite2.set_texture(powerUpBar_EmptyTexture)
+		
 	if POWERUPBARLEVEL > 3:
 		$Sprite3.set_texture(powerUpBar_FullTexture)
 	else:
 		$Sprite3.set_texture(powerUpBar_EmptyTexture)
+		
 	if POWERUPBARLEVEL >4:
 		$Sprite4.set_texture(powerUpBar_FullTexture)
 	else:
